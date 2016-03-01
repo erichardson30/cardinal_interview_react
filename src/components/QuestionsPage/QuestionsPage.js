3d66f1631be949b33494b7662e8a7fef7d2ea7ec
Eric <eric.richardson30@gmail.com>
2016-03-01 15:42:56 -0500

    added floating button to questions page to add question

--- src/components/QuestionsPage/QuestionsPage.js
+++ src/components/QuestionsPage/QuestionsPage.js
@@ -14,6 +14,7 @@ import QuestionStore from '../../stores/QuestionStore';
 import QuestionActions from '../../actions/QuestionActions';
 import Loader from 'react-loader';
 import QuestionItem from './QuestionItem';
+import FloatButton from '../UI/FloatButton';
 
 const title = 'Questions';
 
@@ -59,6 +60,7 @@ renderData() {
           <h1>{title}</h1>
             <div>
                 <Loader loaded={this.state.loaded} />
+                <FloatButton />
                 { this.renderData() }
             </div>
         </div>
